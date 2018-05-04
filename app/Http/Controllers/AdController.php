54b9db0c9db09e465a03c19868bf403d01515415
Jonathan Macheret <jonathan.macheret@liip.ch>
2018-05-04 11:25:12 +0200

    Add new ad controller method to create and store

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -3,13 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Ad;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 
 class AdController extends Controller
 {
+    use ValidatesRequests;
+
     public function index()
     {
         $ads = Ad::with('platforms')->get();
-        return view('pages.ads', compact('ads'));
+        return view('pages.ads.list', compact('ads'));
+    }
+
+    public function create()
+    {
+        return view('pages.ads.create');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'img_url' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        Ad::create($validatedData);
+
+        return redirect()->route('ads');
     }
 }
