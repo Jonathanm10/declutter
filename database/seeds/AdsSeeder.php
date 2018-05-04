04fff7bbf2a9fc68e5f354e30af9a885f8555689
Jonathan Macheret <jonathan.macheret@liip.ch>
2018-05-04 09:28:03 +0200

    Separate seeders

--- database/seeds/AdsSeeder.php
+++ database/seeds/AdsSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class DummyValuesSeeder extends Seeder
+class AdsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -12,20 +12,11 @@ class DummyValuesSeeder extends Seeder
      */
     public function run()
     {
-        $adId = DB::table('ads')->insertGetId([
+        DB::table('ads')->insert([
             'title' => 'My testing title ad',
             'description' => 'My could be long testing description',
             'img_url' => 'https://news.nationalgeographic.com/content/dam/news/photos/000/755/75552.adapt.1900.1.jpg',
             'price' => 34.45,
-        ]);
-
-        $platformId = DB::table('platforms')->insertGetId([
-            'type' => 'Twitter',
-        ]);
-
-        DB::table('ad_platforms')->insert([
-            'ad_id' => $adId,
-            'platform_id' => $platformId,
         ]);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call([
-             DummyValuesSeeder::class,
+             AdsSeeder::class,
+             PlatformsSeeder::class,
+             AdPlatformsSeeder::class,
          ]);
     }
 }
