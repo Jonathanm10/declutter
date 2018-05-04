60436f78bf83a5a908138b9dd000a202392f3427
Jonathan Macheret <jonathan.macheret@liip.ch>
2018-05-04 11:22:36 +0200

    Add new ads routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'AdController@index')->name('ads');
+Route::get('/create', 'AdController@create')->name('ads_create');
+Route::post('/store', 'AdController@store')->name('ads_store');
