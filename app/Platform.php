5750ffe5495f64fb3d552612c3a41420cd483048
Jonathan Macheret <jonathan.macheret@liip.ch>
2018-05-08 11:40:51 +0200

    Update platform get/set config attribute and remove timestamps

--- app/Platform.php
+++ app/Platform.php
@@ -10,10 +10,21 @@ use Illuminate\Database\Eloquent\Model;
  * @mixin \Eloquent
  * @property int $id
  * @property string $type
+ * @property string $config
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Platform whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Platform whereType($value)
  */
 class Platform extends Model
 {
-    //
+    public $timestamps = false;
+
+    public function getConfigAttribute($value)
+    {
+        return unserialize($value);
+    }
+
+    public function setConfigAttribute($value)
+    {
+        $this->attributes['config'] = serialize($value);
+    }
 }
