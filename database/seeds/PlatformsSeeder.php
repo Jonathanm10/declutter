6e0c4c80da56105513a1deb8b7947dfa08dc352b
Jonathan Macheret <jonathan.macheret@liip.ch>
2018-05-08 11:23:00 +0200

    Add new config field to platform table and add it too to the seeder

--- database/seeds/PlatformsSeeder.php
+++ database/seeds/PlatformsSeeder.php
@@ -14,6 +14,7 @@ class PlatformsSeeder extends Seeder
     {
         DB::table('platforms')->insert([
             'type' => 'Testingplatform',
+            'config' => serialize(['username' => 'usernametest', 'password' => 'passwordtest']),
         ]);
     }
 }
