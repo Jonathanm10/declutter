6cec75e3605078af45f0c22ae9a34c4157f4432c
Jonathan Macheret <jonathan.macheret@liip.ch>
2018-05-09 12:01:27 +0200

    Update ide helper generation files

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-05-09 06:50:13.
+    * Generated on 2018-05-09 08:50:08.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.20 on 2018-05-09 06:50:08.
+ * Generated for Laravel 5.6.20 on 2018-05-09 08:50:17.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
